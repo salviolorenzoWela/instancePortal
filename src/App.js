6e42c7e634cce7d4bf464c31eec396e1359cbd85
salviolorenzoWela <lorenzo@getwela.com>
2019-08-18 15:42:42 -0400

    updating folder structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Header from './components/header/header';
+import Footer from './components/footer/footer';
+import Home from './components/home/home';
+import Instances from './components/instances/instances';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <div className="App">
+          <Header />
+          <Route path='/' exact component={Home}></Route>
+          <Route path='/instances' component={Instances}></Route>
+          <Footer />
+        </div>
+      </Router>
+    </>
   );
 }
 
